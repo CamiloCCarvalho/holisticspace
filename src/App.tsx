70dc924e4e3208f37a614065c1aa077ca42bb2a1
Camilo Costa <asfaltorodas@gmail.com>
2023-04-19 12:52:32 -0300

    Att

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,17 @@ function App() {
 
       <div className="normalCard">
         <h2 className="container-fluid m-5">Mais Sobre</h2>
-        <img src="" alt="" />
+        <img className="images"src="/massageImage.webp" alt="Imagem" />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At deserunt nulla similique, natus placeat nobis harum cum, quisquam cumque quaerat asperiores voluptate nisi mollitia odio. Cumque dolorem itaque illo vero.</p>
       </div>
       <div className="hightlightCard img">
         <h2 className="container-fluid m-5">Ilustração</h2>
+        <img className="images"src="/massageImage.webp" alt="Imagem" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At deserunt nulla similique, natus placeat nobis harum cum, quisquam cumque quaerat asperiores voluptate nisi mollitia odio. Cumque dolorem itaque illo vero.</p>
+      </div>
+      <div className="hightlightCard img color">
+        <h2 className="container-fluid m-5">Ilustração</h2>
+        <img className="images"src="/massageImage.webp" alt="Imagem" />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At deserunt nulla similique, natus placeat nobis harum cum, quisquam cumque quaerat asperiores voluptate nisi mollitia odio. Cumque dolorem itaque illo vero.</p>
       </div>
       <footer className="footer">
